68c012efe7d29d331f1ab00402da45b1dda0da40
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-09 01:31:36 +0300

    adding EditQuote

--- src/main/java/ru/acorn/quotation/controller/ScoreController.java
+++ src/main/java/ru/acorn/quotation/controller/ScoreController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.acorn.quotation.service.QuoteService;
 import ru.acorn.quotation.service.ScoreService;
 
 @RestController
