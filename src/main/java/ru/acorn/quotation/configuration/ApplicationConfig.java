f6a4621bc4473eabadf0130beebfe8e587d06c00
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-09 12:23:21 +0300

    adding appConfig

---
+++ src/main/java/ru/acorn/quotation/configuration/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package ru.acorn.quotation.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.acorn.quotation.repository.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User is not found"));
+    }
+}
