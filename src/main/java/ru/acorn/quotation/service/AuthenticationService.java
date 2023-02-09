c234ca6b83cf6d5de94e025168905eea29085e62
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-09 13:40:46 +0300

    AuthenticationResponse authenticate

--- src/main/java/ru/acorn/quotation/service/AuthenticationService.java
+++ src/main/java/ru/acorn/quotation/service/AuthenticationService.java
@@ -1,6 +1,8 @@
 package ru.acorn.quotation.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.acorn.quotation.configuration.JwtService;
@@ -11,6 +13,8 @@ import ru.acorn.quotation.entity.Role;
 import ru.acorn.quotation.entity.User;
 import ru.acorn.quotation.repository.UserRepository;
 
+import java.util.TreeSet;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
@@ -18,6 +22,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -35,6 +40,17 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
