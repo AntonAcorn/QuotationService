5f64c14399e86da3c17793f25ad7ba62c620e903
AntonAcorn <ShustikovAnimation@gmail.com>
2023-02-10 15:40:26 +0300

    Refactoring

--- src/main/java/ru/acorn/quotation/service/QuoteService.java
+++ src/main/java/ru/acorn/quotation/service/QuoteService.java
@@ -52,9 +52,9 @@ public class QuoteService {
 
     public List<Quote> getQuotesByTop(Integer page, Integer limit, boolean orderByTop) {
         if (orderByTop) {
-           return quoteRepository.findAll(PageRequest.of
+            return quoteRepository.findAll(PageRequest.of
                     (page, limit, Sort.by(Sort.Direction.DESC, "quoteLike"))).getContent();
-        }else{
+        } else {
             return quoteRepository.findAll(PageRequest.of
                     (page, limit)).getContent();
         }
@@ -64,7 +64,7 @@ public class QuoteService {
         if (orderByFlop) {
             return quoteRepository.findAll(PageRequest.of
                     (page, limit, Sort.by(Sort.Direction.DESC, "quoteDislike"))).getContent();
-        }else{
+        } else {
             return quoteRepository.findAll(PageRequest.of
                     (page, limit)).getContent();
         }
@@ -74,22 +74,21 @@ public class QuoteService {
         return quoteRepository.findAllByOrderByCreationTimeDesc();
     }
 
-    public Optional<Quote> getRandomQuote() {
+    public Optional <Quote> getRandomQuote() {
         var size = quoteRepository.findAll().size();
-        var minValue = 1L;
-        if (size == minValue) {
+
+        var minSize = 1L;
+        if ((long) size == minSize) {
             return quoteRepository.findById(1L);
         } else {
-            var randomId = getRandomNumber(minValue, (long) size);
+            var randomId = getRandomNumber((long) size);
             return quoteRepository.findById(randomId);
         }
     }
 
-    public Long getRandomNumber(Long min, Long max) {
+    public Long getRandomNumber(Long max) {
         var minValue = 1L;
         Random random = new Random();
-        return random.longs(minValue, max)
-                .findFirst()
-                .getAsLong();
+        return random.longs(minValue, max).findFirst().getAsLong();
     }
 }
